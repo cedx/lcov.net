26ee015e982554fdc86486d63d81ed435e7a5409
Cédric Belin <cedric@belin.io>
2025-01-23 19:34:53 +0100

    Add the `LineCoverage` class

--- src/line.cs
+++ src/line.cs
@@ -1,6 +1,30 @@
 namespace belin.lcov;
 
 /// <summary>
+/// Provides the coverage data of lines.
+/// </summary>
+/// <param name="found">The number of lines found.</param>
+/// <param name="hit">The number of lines hit.</param>
+/// <param name="data">The coverage data.</param>
+public class LineCoverage(int found = 0, int hit = 0, IEnumerable<LineData>? data = null) {
+
+	/// <summary>
+	/// The coverage data.
+	/// </summary>
+	public IList<LineData> Data { get; set; } = [.. data ?? []];
+
+	/// <summary>
+	/// The number of lines found.
+	/// </summary>
+	public int Found { get; set; } = found;
+
+	/// <summary>
+	/// The number of lines hit.
+	/// </summary>
+	public int Hit { get; set; } = hit;
+}
+
+/// <summary>
 /// Provides details for line coverage.
 /// </summary>
 /// <param name="LineNumber">The line number.</param>
