d8bcf02d6fe9cb194092378ec37151890fd4bb9f
Cédric Belin <cedric@belin.io>
2025-01-25 00:18:16 +0100

    Use constants instead of static instances

--- src/token.cs
+++ src/token.cs
@@ -8,65 +8,65 @@ public static class Token {
 	/// <summary>
 	/// The coverage data of a branch.
 	/// </summary>
-	public static readonly string BranchData = "BRDA";
+	public const string BranchData = "BRDA";
 
 	/// <summary>
 	/// The number of branches found.
 	/// </summary>
-	public static readonly string BranchesFound = "BRF";
+	public const string BranchesFound = "BRF";
 
 	/// <summary>
 	/// The number of branches hit.
 	/// </summary>
-	public static readonly string BranchesHit = "BRH";
+	public const string BranchesHit = "BRH";
 
 	/// <summary>
 	/// The end of a section.
 	/// </summary>
-	public static readonly string EndOfRecord = "end_of_record";
+	public const string EndOfRecord = "end_of_record";
 
 	/// <summary>
 	/// The coverage data of a function.
 	/// </summary>
-	public static readonly string FunctionData = "FNDA";
+	public const string FunctionData = "FNDA";
 
 	/// <summary>
 	/// A function name.
 	/// </summary>
-	public static readonly string FunctionName = "FN";
+	public const string FunctionName = "FN";
 
 	/// <summary>
 	/// The number of functions found.
 	/// </summary>
-	public static readonly string FunctionsFound = "FNF";
+	public const string FunctionsFound = "FNF";
 
 	/// <summary>
 	/// The number of functions hit.
 	/// </summary>
-	public static readonly string FunctionsHit = "FNH";
+	public const string FunctionsHit = "FNH";
 
 	/// <summary>
 	/// The coverage data of a line.
 	/// </summary>
-	public static readonly string LineData = "DA";
+	public const string LineData = "DA";
 
 	/// <summary>
 	/// The number of lines found.
 	/// </summary>
-	public static readonly string LinesFound = "LF";
+	public const string LinesFound = "LF";
 
 	/// <summary>
 	/// The number of lines hit.
 	/// </summary>
-	public static readonly string LinesHit = "LH";
+	public const string LinesHit = "LH";
 
 	/// <summary>
 	/// The path to a source file.
 	/// </summary>
-	public static readonly string SourceFile = "SF";
+	public const string SourceFile = "SF";
 
 	/// <summary>
 	/// A test name.
 	/// </summary>
-	public static readonly string TestName = "TN";
+	public const string TestName = "TN";
 }
