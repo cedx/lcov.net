093885df2edaf066ea833995442a81782bacec2d
Cédric Belin <cedric@belin.io>
2025-01-25 13:09:46 +0100

    Add some examples

---
+++ example/format_report.cs
@@ -0,0 +1,16 @@
+using System;
+using Belin.Lcov;
+
+// Formats coverage data as LCOV report.
+var lineData = new[] {
+	new LineData { LineNumber = 6, ExecutionCount = 2, Checksum = "PF4Rz2r7RTliO9u6bZ7h6g" },
+	new LineData { LineNumber = 7, ExecutionCount = 2, Checksum = "yGMB6FhEEAd8OyASe3Ni1w" }
+};
+
+var sourceFile = new SourceFile("/home/cedx/lcov.cs/fixture.cs") {
+	Functions = new FunctionCoverage { Found = 1, Hit = 1 },
+	Lines = new LineCoverage { Found = 2, Hit = 2, Data = lineData }
+};
+
+var report = new Report("Example", [sourceFile]);
+Console.WriteLine(report);
---
+++ example/parse_report.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using Belin.Lcov;
+
+// Parses a LCOV report to coverage data.
+try {
+	var report = Report.Parse(File.ReadAllText("/path/to/lcov.info"));
+	Console.WriteLine($"The coverage report contains {report.SourceFiles.Count} source files:");
+
+	var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true };
+	Console.WriteLine(JsonSerializer.Serialize(report, options));
+}
+catch (FormatException error) {
+	Console.WriteLine(error.Message);
+}
