628e54616717ff564e9b170f5a3ece631e8d7bc6
Cédric Belin <cedric@belin.io>
2025-04-25 11:46:16 +0200

    Update the tests

--- test/ReportTest.cs
+++ test/ReportTest.cs
@@ -20,7 +20,7 @@ public sealed class ReportTest {
 	[TestMethod]
 	public void Parse() {
 		var report = Report.Parse(coverage);
-		AreEqual(3, report.SourceFiles.Count);
+		HasCount(3, report.SourceFiles);
 		AreEqual("/home/cedx/lcov.net/fixture.cs", report.SourceFiles[0].Path);
 		AreEqual("/home/cedx/lcov.net/func1.cs", report.SourceFiles[1].Path);
 		AreEqual("/home/cedx/lcov.net/func2.cs", report.SourceFiles[2].Path);
@@ -28,19 +28,19 @@ public sealed class ReportTest {
 		var branches = report.SourceFiles[1].Branches!;
 		AreEqual(4, branches.Found);
 		AreEqual(4, branches.Hit);
-		AreEqual(4, branches.Data.Count);
+		HasCount(4, branches.Data);
 		AreEqual(8, branches.Data[0].LineNumber);
 
 		var functions = report.SourceFiles[1].Functions!;
 		AreEqual(1, functions.Found);
 		AreEqual(1, functions.Hit);
-		AreEqual(1, functions.Data.Count);
+		HasCount(1, functions.Data);
 		AreEqual("func1", functions.Data[0].FunctionName);
 
 		var lines = report.SourceFiles[1].Lines!;
 		AreEqual(9, lines.Found);
 		AreEqual(9, lines.Hit);
-		AreEqual(9, lines.Data.Count);
+		HasCount(9, lines.Data);
 		AreEqual("5kX7OTfHFcjnS98fjeVqNA", lines.Data[0].Checksum);
 
 		ThrowsExactly<FormatException>(() => Report.Parse("TN:Example"));
