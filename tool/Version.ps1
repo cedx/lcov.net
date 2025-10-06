41963bd5272e07201a6e4be4a18f111ba68fc344
Cédric Belin <cedx@outlook.com>
2025-10-06 12:29:27 +0200

    Move the "Package.xml" file to the project root [skip ci]

--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,3 +1,3 @@
 Write-Host "Updating the version number in the sources..."
-$version = [xml] (Get-Content "etc/Package.xml") | Select-Xml "//Version"
+$version = [xml] (Get-Content "Package.xml") | Select-Xml "//Version"
 foreach ($item in Get-ChildItem "*/*.csproj") { (Get-Content $item) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Out-File $item }
