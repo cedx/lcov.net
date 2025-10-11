96ce57884e24597c0b4209d4772ae38b4e06ea55
Cédric Belin <cedx@outlook.com>
2025-10-11 18:32:35 +0200

    Replace the "Package.xml" file by a PowerShell data file [skip ci]

--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,5 +1,5 @@
 Write-Output "Updating the version number in the sources..."
-$version = (Select-Xml "//Version" Package.xml).Node.InnerText
+$version = (Import-PowerShellDataFile "Package.psd1").Version
 foreach ($item in Get-ChildItem "*/*.csproj") {
 	(Get-Content $item) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Out-File $item
 }
