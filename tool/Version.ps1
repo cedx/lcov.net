0e2144c0fee8ea9bec166f137626478fce2157b4
Cédric Belin <cedx@outlook.com>
2025-10-13 11:21:45 +0200

    Update the build system [skip ci]

--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,5 +1,5 @@
 Write-Output "Updating the version number in the sources..."
-$version = (Import-PowerShellDataFile "Package.psd1").Version
+$version = (Import-PowerShellDataFile "Lcov.psd1").ModuleVersion
 foreach ($item in Get-ChildItem "*/*.csproj") {
 	(Get-Content $item) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Out-File $item
 }
