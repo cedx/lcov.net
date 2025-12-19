abb7de3bcd38c152f3d3c5d477f065b393c18a1c
Cédric Belin <cedx@outlook.com>
2025-12-19 23:15:49 +0100

    Add the `New-BranchData` cmdlet

---
+++ src/Lcov.Cmdlets/New-BranchData.cs
@@ -0,0 +1,42 @@
+namespace Belin.Lcov.Cmdlets;
+
+/// <summary>
+/// Creates new branch data.
+/// </summary>
+[Cmdlet(VerbsCommon.New, "BranchData"), OutputType(typeof(BranchData))]
+public class NewBranchDataCommand: Cmdlet {
+
+	/// <summary>
+	/// The block number.
+	/// </summary>
+	[Parameter, ValidateRange(ValidateRangeKind.NonNegative)]
+	public int BlockNumber { get; set; }
+
+	/// <summary>
+	/// The branch number.
+	/// </summary>
+	[Parameter, ValidateRange(ValidateRangeKind.NonNegative)]
+	public int BranchNumber { get; set; }
+
+	/// <summary>
+	/// The line number.
+	/// </summary>
+	[Parameter, ValidateRange(ValidateRangeKind.NonNegative)]
+	public int LineNumber { get; set; }
+	
+	/// <summary>
+	/// A number indicating how often this branch was taken.
+	/// </summary>
+	[Parameter, ValidateRange(ValidateRangeKind.NonNegative)]
+	public int Taken { get; set; }
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() => WriteObject(new BranchData {
+		BlockNumber = BlockNumber,
+		BranchNumber = BranchNumber,
+		LineNumber = LineNumber,
+		Taken = Taken
+	});
+}
---
+++ src/Lcov/BranchData.cs
@@ -0,0 +1,20 @@
+namespace Belin.Lcov;
+
+/// <summary>
+/// Provides details for branch coverage.
+/// </summary>
+/// <param name="LineNumber">The line number.</param>
+/// <param name="BlockNumber">The block number.</param>
+/// <param name="BranchNumber">The branch number.</param>
+/// <param name="Taken">A number indicating how often this branch was taken.</param>
+public sealed record BranchData(int LineNumber = 0, int BlockNumber = 0, int BranchNumber = 0, int Taken = 0) {
+
+	/// <summary>
+	/// Returns a string representation of this object.
+	/// </summary>
+	/// <returns>The string representation of this object.</returns>
+	public override string ToString() {
+		var value = $"{Tokens.BranchData}:{LineNumber},{BlockNumber},{BranchNumber}";
+		return Taken > 0 ? $"{value},{Taken}" : $"{value},-";
+	}
+}
