6e5f7891e03b479e763a0c9fdc681442ab4eee34
Cédric Belin <cedx@outlook.com>
2025-12-19 22:27:53 +0100

    Add the `New-LineCoverage` cmdlet

---
+++ src/Lcov.Cmdlets/New-LineCoverage.cs
@@ -0,0 +1,35 @@
+namespace Belin.Lcov.Cmdlets;
+
+/// <summary>
+/// Creates a new line coverage.
+/// </summary>
+[Cmdlet(VerbsCommon.New, "LineCoverage"), OutputType(typeof(LineCoverage))]
+public class NewLineCoverageCommand: Cmdlet {
+
+	/// <summary>
+	/// The coverage data.
+	/// </summary>
+	[Parameter]
+	public LineData[] Data { get; set; } = [];
+		
+	/// <summary>
+	/// The number of lines found.
+	/// </summary>
+	[Parameter, ValidateRange(ValidateRangeKind.NonNegative)]
+	public int Found { get; set; }
+
+	/// <summary>
+	/// The number of lines hit.
+	/// </summary>
+	[Parameter, ValidateRange(ValidateRangeKind.NonNegative)]
+	public int Hit { get; set; }
+
+	/// <summary>
+	/// Performs execution of this command.
+	/// </summary>
+	protected override void ProcessRecord() => WriteObject(new LineCoverage {
+		Data = Data,
+		Found = Found,
+		Hit = Hit
+	});
+}
