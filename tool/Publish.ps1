1df8a57e0165cea15901dcfb784378f9a8d4cdb9
Cédric Belin <cedx@outlook.com>
2026-04-03 19:19:02 +0200

    Fix the "Publish" script [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -28,4 +28,4 @@ $module.RequiredAssemblies | Copy-Item -Destination $output/bin
 $output = "var/PSGallery"
 New-Item $output -ItemType Directory | Out-Null
 Compress-PSResource var/PSModule $output
-Get-Item "$output/*.nupkg" | ForEach-Object { Publish-PSResource -ApiKey $Env:PSGALLERY_API_KEY -NupkgPath $_ }
+Get-Item "$output/*.nupkg" | ForEach-Object { Publish-PSResource -ApiKey $Env:PSGALLERY_API_KEY -NupkgPath $_ -Repository PSGallery }
