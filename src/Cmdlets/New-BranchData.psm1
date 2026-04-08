151da280b2bf06451424dcc18f0536045da9f5b2
Cédric Belin <cedx@outlook.com>
2026-04-08 10:03:22 +0200

    Port the `New-BranchData` cmdlet

---
+++ src/Cmdlets/New-BranchData.psm1
@@ -0,0 +1,36 @@
+using namespace Belin.Lcov
+
+<#
+.SYNOPSIS
+	Creates new branch data.
+.OUTPUTS
+	The newly created branch data.
+#>
+function New-BranchData {
+	[CmdletBinding()]
+	[OutputType([Belin.Lcov.BranchData])]
+	param (
+		# The block number.
+		[ValidateRange("NonNegative")]
+		[int] $BlockNumber,
+
+		# The branch number.
+		[ValidateRange("NonNegative")]
+		[int] $BranchNumber,
+
+		# The line number.
+		[ValidateRange("NonNegative")]
+		[int] $LineNumber,
+
+		# A number indicating how often this branch was taken.
+		[ValidateRange("NonNegative")]
+		[int] $Taken
+	)
+
+	[BranchData]@{
+		BlockNumber = $BlockNumber
+		BranchNumber = $BranchNumber
+		LineNumber = $LineNumber
+		Taken = $Taken
+	}
+}
