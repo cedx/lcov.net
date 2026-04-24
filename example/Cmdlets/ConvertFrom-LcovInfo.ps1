af3633fefa0ecbe1af170780e868064b0dc98dd1
Cédric Belin <cedx@outlook.com>
2026-04-24 11:28:32 +0200

    Remove the cmdlets

--- example/Cmdlets/ConvertFrom-LcovInfo.ps1
+++
@@ -1,9 +0,0 @@
-<#
-.SYNOPSIS
-	Parses a LCOV report to coverage data.
-#>
-Import-Module Belin.Lcov
-
-$report = ConvertFrom-LcovInfo "/path/to/lcov.info"
-Write-Output "The coverage report contains $($report.SourceFiles.Count) source files:"
-Write-Output (ConvertTo-Json $report -Depth 5)
